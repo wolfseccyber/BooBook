aaf02d5dd35baa351d7e524b6f67472e11c3a091
OfficialCaptain <hakantheamazing@gmail.com>
2020-09-17 19:03:00 -0600

    added profile, login/logout/register.

--- boobook/settings.py
+++ boobook/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-CRISPY_TEMPLATE_PACK='bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
+
 
 django_heroku.settings(locals())
